87c717451640a0962ccbb2138d2e9c24d1cea52b
stephen nene <stevekid705@gmail.com>
2023-09-12 13:20:26 +0300

    nice ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,56 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [link, setLink] = useState("");
+  const [image, setImage] = useState("https://placehold.co/600x400.png");
+  const [error, setError] = useState("");
 
   async function greet() {
+    if (!link) {
+      setError("Link cannot be empty");
+      return;
+    }
+
+    setError(""); // Clear any previous errors
+
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+    setGreetMsg(await invoke("greet", { link }));
   }
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold mb-6">Welcome to Steves-bet bot!</h1>
 
       <form
-        className="row"
         onSubmit={(e) => {
           e.preventDefault();
           greet();
         }}
+        className="flex items-center space-x-2"
       >
         <input
           id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
+          className={`${
+            error ? "border-red " : "border-gray-300"
+          } border p-2 rounded-lg`}
+          onChange={(e) => setLink(e.currentTarget.value)}
+          placeholder="Enter a link..."
         />
-        <button type="submit">Greet</button>
+        <button
+          type="submit"
+          className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+        >
+          Scrape
+        </button>
       </form>
 
-      <p>{greetMsg}</p>
+      {error && <p className="text-red mt-2">{error}</p>}
+      <p className="mt-4">{greetMsg}</p>
+
+      <img className="my-4" src={image} alt="" />
     </div>
   );
 }
